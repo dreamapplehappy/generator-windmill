82685bacdf63abac2baaf56e9698db14aff57f74
dreamapple <2451731631@qq.com>
2016-04-15 17:29:10 +0800

    添加一些文件

--- generators/app/index.js
+++ generators/app/index.js
@@ -30,21 +30,55 @@ module.exports = yeoman.Base.extend({
 
   writing: {
     config: function() {
+      this.fs.copy(
+        this.templatePath('eslintrc'),
+        this.destinationPath('.eslintrc')
+      );
+      this.fs.copy(
+        this.templatePath('gitignore'),
+        this.destinationPath('.gitignore')
+      );
+      this.fs.copy(
+        this.templatePath('stylelintrc'),
+        this.destinationPath('.stylelintrc')
+      );
+      this.fs.copy(
+        this.templatePath('_gulpfile.babel.js'),
+        this.destinationPath('gulpfile.babel.js')
+      );
+      this.fs.copy(
+        this.templatePath('_gulpfile.es5.js'),
+        this.destinationPath('gulpfile.es5.js')
+      );
+      this.fs.copy(
+        this.templatePath('_karma.conf.js'),
+        this.destinationPath('karma.conf.js')
+      );
+      this.fs.copy(
+        this.templatePath('_protractor.conf.js'),
+        this.destinationPath('protractor.conf.js')
+      );
+      this.fs.copy(
+        this.templatePath('_tool.config.js'),
+        this.destinationPath('tool.config.js')
+      );
+      this.fs.copy(
+        this.templatePath('_webpack.config.js'),
+        this.destinationPath('webpack.config.js')
+      );
+      this.fs.copy(
+        this.templatePath('_webpack.dev.config.js'),
+        this.destinationPath('webpack.dev.config.js')
+      );
+      this.fs.copy(
+        this.templatePath('_webpack.dist.config.js'),
+        this.destinationPath('webpack.dist.config.js')
+      );
       this.fs.copyTpl(
         this.templatePath('_package.json'),
         this.destinationPath('package.json'), {
-          name: this.props.name
+          name: this.props.appName
         }
-      );
-      this.fs.copyTpl(
-        this.templatePath('_bower.json'),
-        this.destinationPath('bower.json'), {
-          name: this.props.name
-        }
-      );
-      this.fs.copy(
-        this.templatePath('bowerrc'),
-        this.destinationPath('.bowerrc')
       );
     },
   },
